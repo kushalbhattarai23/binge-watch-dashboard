38e2acfb76753fbff823da06c74257b950b1fe38
Dell <Dell@KUSHALBHATTARAI>
2025-06-23 17:21:47 +0545

    deleting redundant application.

--- src/apps/settlegara/src/pages/BillsPage.tsx
+++
@@ -1,7 +0,0 @@
-
-import React from 'react';
-import { BillsListPage } from './BillsListPage';
-
-export const BillsPage: React.FC = () => {
-  return <BillsListPage />;
-};
--- src/apps/settlegara/src/pages/NetworksPage.tsx
+++
@@ -1,7 +0,0 @@
-
-import React from 'react';
-import { NetworksListPage } from './NetworksListPage';
-
-export const NetworksPage: React.FC = () => {
-  return <NetworksListPage />;
-};
